ef05fc37158ff8c8c7f91fd9a87fe0a9ffba9f81
FebinSanthosh <84325065+FebinSanthosh@users.noreply.github.com>
2021-05-17 20:26:45 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,11 @@
+function purple_btn(){
+  document.body.style.background="purple"  
+}
+
+function green_btn(){
+    document.body.style.background="greenyellow"   
+}
+
+function yellow_btn(){
+    document.body.style.background="yellow"  
+}
